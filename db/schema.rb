1fc1ba7c3568a6f9557f216a2b40822c036957be
Iramal <9588504@gmail.com>
2020-07-15 20:41:51 +0500

    add demo data to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_181535) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "role"
+    t.string "role", default: "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["username"], name: "index_users_on_username", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Создаем пользователей
+user_1 = User.create(username: 'Ivan', role: 'admin')
+user_2 = User.create(username: 'Jhon')
+user_3 = User.create(username: 'Fiona')
+user_4 = User.create(username: 'Harold')
+
+# Создаем пустую группу
+group_1 = Group.create(name: 'Newbies')
+
+# В созданную группу добавляем пользователей
+group_1.users << user_2 << user_3
+
+# Создаем курс и тест
+course = Course.create(name: 'Algebra')
+course2 = Course.create(name: 'Geo')
+quiz = Quiz.create(name: 'History')
+
+# Создаем учебное задание
+EducationalAssignment.create(assignee: user_4, educational: quiz)
+EducationalAssignment.create(assignee: group_1, educational: course)
+EducationalAssignment.create(assignee: user_2, educational: course2)
+
+
+# Пользователь user_4 завершает тест
+Completion.create(user: user_4, educational: quiz)
+Completion.create(user: user_2, educational: course2)
